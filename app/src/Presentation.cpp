ddd8df4884596002fee936ba855a35f133e4f5b5
Karol Maksymowicz <ja@loliting.dev>
2024-01-21 00:53:39 +0100

    app: Implement XML parsing with Text Element

--- app/src/Presentation.cpp
+++ app/src/Presentation.cpp
@@ -1,14 +1,127 @@
 #include "Presentation.hpp"
 
 #include <QtCore/QFile>
-#include <QtCore/QDebug>
+#include <QtGui/QResizeEvent>
+
+#include <string>
+#include <sstream>
 
 #include <zip.h>
+#include "third-party/RapidXml/rapidxml.hpp"
+#include "third-party/RapidXml/rapidxml_print.hpp"
 
 // Size of unzip buffor
-#define BUFFOR_SIZE 1024
+#define BUFFOR_SZ 1024
+#define TEXT_NODE_SZ sizeof("<Text>")
 
-void Presentation::decompressVslidesArchive(QString path){
+using namespace rapidxml;
+
+static void parseXmlDimensions(xml_node<char>* xmlNode, PresentationElement* e) {
+    qreal x = 0, y = 0, w = 0, h = 0;
+    
+    xml_attribute<char>* attrib = nullptr;
+    
+    attrib = xmlNode->first_attribute("x", 0UL, false);
+    if(attrib != nullptr){
+        x = QString(attrib->value()).toDouble() / 100;
+    }
+    attrib = xmlNode->first_attribute("y", 0UL, false);
+    if(attrib != nullptr){
+        y = QString(attrib->value()).toDouble() / 100;
+    }
+    attrib = xmlNode->first_attribute("width", 0UL, false);
+    if(attrib != nullptr){
+        w = QString(attrib->value()).toDouble() / 100;
+    }
+    attrib = xmlNode->first_attribute("height", 0UL, false);
+    if(attrib != nullptr){
+        h = QString(attrib->value()).toDouble() / 100;
+    }
+    e->setX(x);
+    e->setY(y);
+    e->setWidth(w);
+    e->setHeight(h);
+}
+
+void PresentationElement::recalculateRealDim() {
+    if(m_widget == nullptr){
+        return;
+    }
+
+    QWidget* parent = m_widget->parentWidget();
+    if(parent == nullptr){
+        return;
+    }
+
+    m_widget->resize(width() * parent->width(),
+        height() * parent->height()
+    );
+    m_widget->move(x() * parent->width() - m_widget->width() / 2,
+        y() * parent->height() - m_widget->height() / 2
+    );
+}
+
+PresentationElement::PresentationElement() {
+    connect(this, SIGNAL(widgetSet(void)), this, SLOT(recalculateRealDim(void)));
+    connect(this, SIGNAL(sizeChanged(void)), this, SLOT(recalculateRealDim(void)));
+    connect(this, SIGNAL(positionChanged(void)), this, SLOT(recalculateRealDim(void)));
+}
+
+PresentationElement::PresentationElement(qreal x, qreal y, qreal height, qreal width) {
+    PresentationElement();
+    m_pos = QPointF(x, y);
+    m_size = QSizeF(width, height);
+}
+
+void PresentationElement::setWidget(QWidget* w) {
+    assert(m_widget == nullptr);
+    m_widget = w;
+
+    QWidget* parent = m_widget->parentWidget();
+    assert(parent != nullptr);
+
+    connect(parent, SIGNAL(resize(int, int)), this, SLOT(recalculateRealDim(void)));
+}
+
+PresentationTextBoxElement::PresentationTextBoxElement(QWidget* parent, QString text)
+    : PresentationElement()
+{
+    m_widget = new QLabel(parent);
+    setWidget(m_widget);
+
+    m_widget->setTextFormat(Qt::RichText);
+    m_widget->setText(text);
+    m_widget->show();
+}
+
+PresentationTextBoxElement::PresentationTextBoxElement(QWidget* parent, qreal x, qreal y, qreal height, qreal width, QString text)
+    : PresentationElement(x, y, width, height)
+{
+    PresentationTextBoxElement(parent, text);
+}
+
+PresentationTextBoxElement::~PresentationTextBoxElement() {
+    delete m_widget;
+}
+
+PresentationSlide::PresentationSlide(QString bg) {
+    m_bg = bg;
+    setAutoFillBackground(true);
+    setPalette(QPalette(QColor(m_bg)));
+}
+
+PresentationSlide::~PresentationSlide() {
+    for(auto element : m_elements){
+        delete element;
+    }
+}
+
+void PresentationSlide::resizeEvent(QResizeEvent *event) {
+    emit resize(event->size().width(), event->size().height());
+    QLabel::resizeEvent(event);
+}
+
+void Presentation::decompressVslidesArchive(QString path) {
     zip_t* zipArchive = nullptr;
     zip_file_t* zf;
     zip_stat_t zStat;
@@ -50,11 +163,11 @@ void Presentation::decompressVslidesArchive(QString path){
                 throw PresentationException("Failed to decompress '" + path + "'");
             }
 
-            char buffor[BUFFOR_SIZE];
+            char buffor[BUFFOR_SZ];
             ssize_t sum = 0;
             ssize_t length;
             while(sum < zStat.size){
-                if((length = zip_fread(zf, buffor, BUFFOR_SIZE)) == -1){
+                if((length = zip_fread(zf, buffor, BUFFOR_SZ)) == -1){
                     file.close();
                     zip_close(zipArchive);
                     throw PresentationException("Failed to decompress '" + path + "'");
@@ -66,6 +179,82 @@ void Presentation::decompressVslidesArchive(QString path){
         }
     }
     zip_close(zipArchive);
+}
+
+void Presentation::parseXml() {
+    QFile rootXml("root.xml");
+
+    if(rootXml.open(QIODevice::ReadOnly) == false){
+        throw PresentationException("File root.xml does not exists inside the archive");
+    }
+    
+    QByteArray ba = rootXml.readAll();
+    
+    xml_document<char> xmlDoc;
+    try{
+        xmlDoc.parse<parse_trim_whitespace | parse_normalize_whitespace>(ba.data());
+    }
+    catch(parse_error& e){
+        throw PresentationException("Failed to parse root.xml: " + QString(e.what()));
+    }
+
+    xml_node<char>* rootNode = xmlDoc.first_node("Presentation", 0UL, false);
+    if(rootNode == nullptr){
+        QString exceptionStr = "Failed to parse root.xml: ";
+        exceptionStr += "\"<Presentation>\" Node does not exist";
+        throw PresentationException(exceptionStr);
+    }
+
+    xml_attribute<char>* titleAttribute;
+    titleAttribute = rootNode->first_attribute("title", 0UL, false);
+    if(titleAttribute != nullptr){
+        m_title = QString(titleAttribute->value());
+    }
+
+
+    xml_node<char>* slideNode = rootNode->first_node("Slide", 0UL, false);
+    xml_attribute<char>* bgAttribute;
+    
+    xml_node<char>* textBoxNode;
+    xml_node<char>* textNode;
+
+    if(slideNode == nullptr){
+        QString exceptionStr = "Failed to parse root.xml: ";
+        exceptionStr += "\"<Presentation>\" node have to contain at least one \"<Slide>\" node";
+        throw PresentationException(exceptionStr);
+    }
+    while(slideNode){
+        PresentationSlide* slide = nullptr;
+
+        bgAttribute = slideNode->first_attribute("bg", 0UL, false);
+        if(bgAttribute){
+            slide = new PresentationSlide(bgAttribute->value());
+        }
+        else{
+            slide = new PresentationSlide();
+        }
+        
+        textBoxNode = slideNode->first_node("TextBox", 0UL, false);
+        while(textBoxNode){
+            textNode = textBoxNode->first_node("Text", 0UL, false);
+            std::stringstream textNodeSs;
+            textNodeSs << *textNode;
+            std::string text = textNodeSs.str();
+            text = text.substr(TEXT_NODE_SZ, text.length() - TEXT_NODE_SZ * 2);
+
+            PresentationTextBoxElement* tB = nullptr;
+            tB = new PresentationTextBoxElement(slide, QString::fromStdString(text));
+            parseXmlDimensions(textBoxNode, tB);
+
+            slide->m_elements.append(tB);
+            textBoxNode = textBoxNode->next_sibling("TextBox", 0UL, false);
+        }
+
+        m_slides.append(slide);
+        slideNode = slideNode->next_sibling("Slide", 0UL, false);
+    }
+
+    rootXml.close();
 }
 
 Presentation::Presentation(QString path) {
@@ -83,14 +272,19 @@ Presentation::Presentation(QString path) {
 
     try{
         decompressVslidesArchive(path);
+        parseXml();
     }
     catch(PresentationException &e){
         QDir::setCurrent(oldCwd);
         m_tmpDir.remove();
         throw;
     }
+    QDir::setCurrent(oldCwd);
 }
 
 Presentation::~Presentation() {
     m_tmpDir.remove();
+    for(auto slide : m_slides){
+        delete slide;
+    }
 }
--- app/src/Presentation.hpp
+++ app/src/Presentation.hpp
@@ -1,6 +1,9 @@
 #ifndef PRESENTATION_HPP
 #define PRESENTATION_HPP
 
+#include <QtCore/QObject>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QLabel>
 #include <QtCore/QTemporaryDir>
 
 #include <exception>
@@ -15,12 +18,95 @@ private:
     std::string m_what;
 };
 
+
+class PresentationElement : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QWidget* widget READ widget WRITE setWidget NOTIFY widgetSet)
+   
+    Q_PROPERTY(qreal x READ x WRITE setX NOTIFY positionChanged)
+    Q_PROPERTY(qreal y READ y WRITE setY NOTIFY positionChanged)
+    Q_PROPERTY(QPointF pos READ pos WRITE setPos NOTIFY positionChanged)
+
+    Q_PROPERTY(qreal width READ width WRITE setWidth NOTIFY sizeChanged)
+    Q_PROPERTY(qreal height READ height WRITE setHeight NOTIFY sizeChanged)
+    Q_PROPERTY(QSizeF size READ size WRITE setSize NOTIFY sizeChanged)
+public:
+    PresentationElement();
+    PresentationElement(qreal x, qreal y, qreal width, qreal height);
+
+    QWidget* widget() const { return m_widget; }
+    void setWidget(QWidget* w);
+
+    virtual void setX(qreal x) { m_pos.setX(x); }
+    virtual void setY(qreal y) { m_pos.setY(y); }
+    virtual void setPos(QPointF pos) { m_pos = pos; }
+
+    virtual void setWidth(qreal width) { m_size.setWidth(width); }
+    virtual void setHeight(qreal height) { m_size.setHeight(height); }
+    virtual void setSize(QSizeF size) { m_size = size; }
+
+    virtual qreal x() const { return m_pos.x(); }
+    virtual qreal y() const  { return m_pos.y(); }
+    virtual QPointF pos() const { return m_pos; }
+
+    virtual qreal width() const { return m_size.width(); }
+    virtual qreal height() const { return m_size.height(); }
+    virtual QSizeF size() const { return m_size; }
+signals:
+    void positionChanged();
+    void sizeChanged();
+    void widgetSet();
+private slots:
+    virtual void recalculateRealDim();
+private:
+    QSizeF m_size = QSizeF();
+    QPointF m_pos = QPointF();
+    QWidget* m_widget = Q_NULLPTR;
+};
+
+class PresentationTextBoxElement : public PresentationElement
+{
+    Q_OBJECT
+public:
+    PresentationTextBoxElement(QWidget* parent, QString text);
+    PresentationTextBoxElement(QWidget* parent, qreal x, qreal y, qreal width, qreal height, QString text);
+    ~PresentationTextBoxElement();
+private:
+    QLabel* m_widget;
+};
+
+
+class PresentationSlide : public QLabel 
+{
+    Q_OBJECT
+public:
+    PresentationSlide(QString bg = QString());
+    ~PresentationSlide();
+signals:
+    void resize(int w, int h);
+private:
+    QString m_bg;
+    QList<PresentationElement*> m_elements;
+    
+protected:
+    void resizeEvent(QResizeEvent *event);
+
+    friend class Presentation;
+};
+
+
 struct Presentation{
 public:
     Presentation(QString path);
     ~Presentation();
 private:
     void decompressVslidesArchive(QString path);
+    void parseXml();
+public:
+    QString m_title;
+    QList<PresentationSlide*> m_slides;
 private:
     QTemporaryDir m_tmpDir = QTemporaryDir();
 };
