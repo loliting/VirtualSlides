6d021d9c8b4b8076f6221f1715460da3a2bbaabe
Karol Maksymowicz <ja@loliting.dev>
2024-04-08 21:41:06 +0200

    start working on implementing new guest-host bridge
    Implemented function to get CID of local machine.
    Moved all of old guest-host communication into legacy-host-bridge feature
    and marked it as default (for now)

--- guest/init/src/host_bridge.rs
+++ guest/init/src/host_bridge.rs
@@ -2,18 +2,34 @@
 
 use std::cmp::max;
 use std::fs::File;
+#[cfg(feature = "legacy-host-bridge")]
 use std::io::{BufRead, BufReader, Write};
 use std::time::{Duration, Instant};
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
+#[cfg(not(feature = "legacy-host-bridge"))]
+use nix::ioctl_read_bad;
 use serde::{Deserialize, Serialize};
 use indicatif::{HumanBytes, ProgressBar};
+#[cfg(not(feature = "legacy-host-bridge"))]
+use std::os::unix::io::AsRawFd; 
 
+#[cfg(feature = "legacy-host-bridge")]
 const WRITE_CONSOLE_PATH: &str = "/dev/hvc0";
+#[cfg(feature = "legacy-host-bridge")]
 const READ_CONSOLE_PATH: &str = "/dev/hvc1";
 
+#[cfg(not(feature = "legacy-host-bridge"))]
+const VSOCK_PATH: &str = "/dev/vsock";
+#[cfg(not(feature = "legacy-host-bridge"))]
+const IOCTL_VM_SOCKETS_GET_LOCAL_CID: usize = 0x7b9;
+
 pub struct HostBridge {
+    #[cfg(feature = "legacy-host-bridge")]
     read_console: File,
-    write_console: File
+    #[cfg(feature = "legacy-host-bridge")]
+    write_console: File,
+    #[cfg(not(feature = "legacy-host-bridge"))]
+    cid: u32
 }
 
 #[derive(Serialize, Deserialize)]
@@ -44,25 +60,59 @@ pub struct Response {
 
 impl HostBridge {
     pub fn new() -> Result<Self> {
-        let read_console = File::options()
-        .read(true)
-        .write(false)
-        .open(READ_CONSOLE_PATH)?;
+        let host_bridge: HostBridge;
 
-        let write_console = File::options()
-        .read(false)
-        .write(true)
-        .open(WRITE_CONSOLE_PATH)?;
+        #[cfg(feature = "legacy-host-bridge")] {
+            let read_console = File::options()
+            .read(true)
+            .write(false)
+            .open(READ_CONSOLE_PATH)?;
+    
+            let write_console = File::options()
+            .read(false)
+            .write(true)
+            .open(WRITE_CONSOLE_PATH)?;
 
-        let host_bridge = HostBridge {
-            read_console: read_console,
-            write_console: write_console
-        };
+            host_bridge = HostBridge {
+                read_console: read_console,
+                write_console: write_console
+            };
+        }
+
+        #[cfg(not(feature = "legacy-host-bridge"))] {
+            host_bridge = HostBridge {
+                cid: HostBridge::get_local_cid()?
+            };
+    
+            println!("Our CID: {}", host_bridge.cid);
+        }
         
         Ok(host_bridge)
     }
 
-    fn write(&mut self, request: RequestType) -> Result<()>  {
+    #[cfg(not(feature = "legacy-host-bridge"))]
+    fn get_local_cid() -> Result<u32> {
+        let mut cid: u32 = 0;
+        let vsock = File::open(VSOCK_PATH)?;
+
+        ioctl_read_bad!(
+            get_local_cid,
+            IOCTL_VM_SOCKETS_GET_LOCAL_CID,
+            u32
+        );
+        
+        unsafe{ get_local_cid(vsock.as_raw_fd(), &mut cid) }?;
+        
+        Ok(cid)
+    }
+
+    #[cfg(not(feature = "legacy-host-bridge"))]
+    fn do_write(&mut self, _request: RequestType) -> Result<()> {
+        Err(anyhow!("Not implemented"))
+    }
+
+    #[cfg(feature = "legacy-host-bridge")]
+    fn do_write(&mut self, request: RequestType) -> Result<()>  {
         let mut message = serde_json::json!({"type": request}).to_string();
         message.push_str("\x1e\n");
         
@@ -71,16 +121,33 @@ impl HostBridge {
         Ok(())
     }
     
-    fn read(&mut self) -> Result<Response> {
+    fn write(&mut self, request: RequestType) -> Result<()> {
+        self.do_write(request)?;
+        Ok(())
+    }
+    
+    #[cfg(not(feature = "legacy-host-bridge"))]
+    fn do_read(&mut self) -> Result<Vec<u8>> {
+        Err(anyhow!("Not implemented"))
+    }
+
+    #[cfg(feature = "legacy-host-bridge")]
+    fn do_read(&mut self) -> Result<Vec<u8>>  {
         let mut reader = BufReader::new(&self.read_console);
+        let mut buf: Vec<u8> = Vec::new();
+        reader.read_until(b'\x1e', &mut buf)?;
+        buf.pop();
+        
+        Ok(buf)
+    }
+
+    fn read(&mut self) -> Result<Response> {
         let now = Instant::now();
         let progress_bar = ProgressBar::new_spinner();
         progress_bar.enable_steady_tick(Duration::from_millis(150));
 
-        let mut buf: Vec<u8> = Vec::new();
-        reader.read_until(b'\x1e', &mut buf)?;
+        let buf: Vec<u8> = self.do_read()?;
         progress_bar.finish();
-        buf.pop();
 
         let response: Response = serde_json::from_slice(&buf)?;
         if response.status == Status::Err {
